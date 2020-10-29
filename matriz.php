2bb5d9881df5bfc80967fa1b980be3b5f2140632
Lucero1867 <luceroolivos@gmail.com>
2020-10-28 21:23:27 -0300

    Ejercicios de matriz

---
+++ matriz.php
@@ -0,0 +1,17 @@
+<?php
+
+// $matriz_int = array(
+//         array(1,2,3,4,5,6,7),
+//         array(8,9,10,11,12,13,14)
+// );
+
+// echo $matriz_int[1][2];
+
+
+
+    for($i = 0; $i <= 6; $i++) {
+        for($j = 1; $j <= 6; $j++) {
+            echo "$i \n";
+            echo $j;
+        }
+    }
