55f552a036a06fff5bba063b556d26cdeae90cb6
lucero1867 <luceroolivos@gmail.com>
2021-01-13 19:22:39 -0300

    Modificacion de ejercicio completa

--- NumerosPrimos_Parte5.php
+++ NumerosPrimos_Parte5.php
@@ -2,22 +2,21 @@
 
 $numero_entero = readline("Ingrese numero: ");
 
-function es_primo($numero_entero){
-    $posibleprimo = 0;
-    for($i = 1; $i <= $numero_entero; $i++){
-        if($numero_entero % $i == 0){
-            $posibleprimo++;
-        }
+// for($i = 2; $i < $numero_entero; $i++){
+//     // $resultado = $numero_entero / $i;
+//     if (($numero_entero % $i) == 0){
+
+//         echo "$i \n";
+//         $i--;
+//     }
+// }
+$i = 2;
+$resulado = $numero_entero;
+
+while ($i < $numero_entero){
+    $i++;
+    if(($numero_entero % $i)== 0){
+        
+        echo "$i \n";
     }
-    return $posibleprimo == 2;
-}
-
-$divisiores = 0;
-
-for($i = 2; $i <= $numero_entero; $i++){
-    $resultado = es_primo($i) / $numero_entero;
-        echo "$i";
-        // $divisores++;
-        // echo $divisores . "\n";
-    
 }
